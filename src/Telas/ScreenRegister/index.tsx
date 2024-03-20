46f948366cc148ac4c675e52eeedd51f240ff1a5
GEOVANI\conta <geosegat@gmail.com>
2024-03-20 17:38:29 -0300

    new

---
+++ src/Telas/ScreenRegister/index.tsx
@@ -0,0 +1,19 @@
+import {Button, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {NavigationProps} from '../../utilitis/types/index';
+
+const ScreenRegister: React.FC<NavigationProps> = ({navigation}) => {
+  const onPress = () => {
+    navigation.navigate('ScreenLogin');
+  };
+  return (
+    <View>
+      <Text>ScreenRegister</Text>
+      <Button title="voltar" onPress={onPress} />
+    </View>
+  );
+};
+
+export default ScreenRegister;
+
+const styles = StyleSheet.create({});
