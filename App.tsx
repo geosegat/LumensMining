0104881b4203483cd2a730df2fa31a30520b07d4
GEOVANI\conta <geosegat@gmail.com>
2024-04-01 21:23:53 -0300

    no message

--- App.tsx
+++ App.tsx
@@ -28,7 +28,7 @@ const App = () => {
           options={{headerShown: false}}
         />
         <Stack.Screen
-          name="ScreenAccount"
+          name="ScreenAccounyt"
           component={ScreenAccount}
           options={{headerShown: true}}
         />
