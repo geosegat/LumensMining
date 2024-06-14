e44d989dd3a4bc95909e27fc8fbb2f737735565f
GEOVANI\conta <geosegat@gmail.com>
2024-06-14 19:24:31 -0300

    new

--- src/Componentes/CardBalanceAccount/index.tsx
+++ src/Componentes/CardBalanceAccount/index.tsx
@@ -1,19 +1,45 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import AppText from '../AppText';
 
 const CardBalanceAccount = () => {
+  const [selectedCurrency, setSelectedCurrency] = useState('BRL');
+
   return (
     <View style={styles.container}>
-      <AppText size="xlarge" color="white" variant="semiBold">
-        Balance
-      </AppText>
+      <View style={{flexDirection: 'row'}}>
+        <TouchableOpacity onPress={() => setSelectedCurrency('BRL')}>
+          <AppText
+            style={
+              selectedCurrency === 'BRL' ? styles.selectedLink : styles.link
+            }
+            size="xlarge"
+            color={selectedCurrency === 'BRL' ? '#fefffe' : '#a8a8a9'}
+            variant="bold">
+            BRL
+          </AppText>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{marginLeft: 10}}
+          onPress={() => setSelectedCurrency('BTC')}>
+          <AppText
+            style={
+              selectedCurrency === 'BTC' ? styles.selectedLink : styles.link
+            }
+            size="xlarge"
+            color={selectedCurrency === 'BTC' ? '#fefffe' : '#a8a8a9'}
+            variant="bold">
+            BTC
+          </AppText>
+        </TouchableOpacity>
+      </View>
+
       <AppText
         style={{marginTop: 5}}
         size="xlarge"
         color="white"
         variant="semiBold">
-        R$: 2.624,52
+        {selectedCurrency === 'BRL' ? 'R$ 41,520.23' : '0.99 BTC'}
       </AppText>
     </View>
   );
@@ -27,5 +53,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#265162',
     borderRadius: 10,
     alignItems: 'center',
+  },
+  link: {
+    textDecorationLine: 'none',
+  },
+  selectedLink: {
+    textDecorationLine: 'underline',
   },
 });
