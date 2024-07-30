ff2585110c36ddd2d291ede389b81d922ee2f405
GEOVANI\conta <geosegat@gmail.com>
2024-07-30 19:52:17 -0300

    no message

--- src/Telas/ScreenTransactions/index.tsx
+++ src/Telas/ScreenTransactions/index.tsx
@@ -5,7 +5,7 @@ import {NavigationProps} from '../../utilitis/types/navigation';
 const ScreenTransactions: React.FC<NavigationProps> = ({navigation}) => {
   return (
     <View style={{flex: 1}}>
-      <View style={{flex: 1, backgroundColor: '#242c33'}}>
+      <View style={{flex: 1, backgroundColor: '#1a1d1b'}}>
         <Text>ScreenTransactions</Text>
       </View>
     </View>
