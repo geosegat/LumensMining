247411205dd6d9146f1f40508f53a71cbd537b2e
GEOVANI\conta <geosegat@gmail.com>
2024-07-31 00:05:45 -0300

    no message

--- src/Telas/ScreenWallet/index.tsx
+++ src/Telas/ScreenWallet/index.tsx
@@ -3,15 +3,22 @@ import React from 'react';
 import {NavigationProps} from '../../utilitis/types/navigation';
 import AppText from '../../Componentes/AppText';
 import CardMoedasWallet from '../../Componentes/CardMoedasWallet';
+import CardBalanceCritpomoeda from '../../Componentes/CardBalanceCriptomoeda';
+import CollectionSvgImg from '../../utilitis/CollectionSvgImg';
 
 const ScreenWallet: React.FC<NavigationProps> = ({navigation}) => {
   return (
-    <View style={{flex: 1, backgroundColor: '#1a1d1b', padding: 15}}>
+    <View style={styles.container}>
+      <View style={{height: 25}} />
       <CardMoedasWallet />
+      <View style={{height: 25}} />
+      <CardBalanceCritpomoeda />
     </View>
   );
 };
 
 export default ScreenWallet;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {flex: 1, backgroundColor: '#1a1d1b', padding: 15},
+});
