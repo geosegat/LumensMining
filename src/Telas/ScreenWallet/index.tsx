8982c45e9898f95d4bcc2f22433ae3c130b0ce81
GEOVANI\conta <geosegat@gmail.com>
2024-07-31 21:16:05 -0300

    ad

--- src/Telas/ScreenWallet/index.tsx
+++ src/Telas/ScreenWallet/index.tsx
@@ -1,35 +1,22 @@
-import {
-  Image,
-  ScrollView,
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-} from 'react-native';
+import {ScrollView, StyleSheet, View} from 'react-native';
 import React, {useState} from 'react';
-import Modal from 'react-native-modal';
 import {NavigationProps} from '../../utilitis/types/navigation';
 import AppText from '../../Componentes/AppText';
 import CardMoedasWallet from '../../Componentes/CardMoedasWallet';
 import CardBalanceCritpomoeda from '../../Componentes/CardBalanceCriptomoeda';
 import CardTransacoes from '../../Componentes/CardTransacoes';
-import CollectionSvgImg from '../../utilitis/CollectionSvgImg';
-import App from '../../../App';
+import CardModalMoeda, {Coin} from '../../Componentes/CardModalMoeda';
 
 const ScreenWallet: React.FC<NavigationProps> = ({navigation}) => {
   const [valorRecebido, setValoRecebido] = useState<number>(0.00006769);
   const [isModalVisible, setModalVisible] = useState(false);
-  const [selectedCoin, setSelectedCoin] = useState('');
-
-  const onPress = () => {
-    setModalVisible(true);
-  };
+  const [selectedCoin, setSelectedCoin] = useState<string>('');
 
   const toggleModal = () => {
     setModalVisible(!isModalVisible);
   };
 
-  const coins = [
+  const coins: Coin[] = [
     {id: '1', name: 'BTC', value: 'bitImg'},
     {id: '2', name: 'ETH', value: 'ethImg'},
     {id: '3', name: 'ARK', value: 'arkImg'},
@@ -40,7 +27,7 @@ const ScreenWallet: React.FC<NavigationProps> = ({navigation}) => {
     <View style={styles.container}>
       <View style={{padding: 10}}>
         <View style={{height: 25}} />
-        <CardMoedasWallet onPress={onPress} />
+        <CardMoedasWallet onPress={toggleModal} />
         <View style={{height: 25}} />
         <CardBalanceCritpomoeda />
         <AppText size="huge" style={styles.transacoes} color="white">
@@ -48,40 +35,18 @@ const ScreenWallet: React.FC<NavigationProps> = ({navigation}) => {
         </AppText>
       </View>
       <ScrollView>
-        <CardTransacoes iconName="ethImg" moeda="eth" />
+        <CardTransacoes iconName="ethImg" moeda="eth" value={0.00136113} />
         <CardTransacoes iconName="arkImg" moeda="ark" value={11.72506138} />
-        <CardTransacoes iconName="bitImg" />
+        <CardTransacoes iconName="bitImg" value={0.16565596} />
         <CardTransacoes iconName="dashImg" moeda="dash" value={valorRecebido} />
         <CardTransacoes />
-        <CardTransacoes />
-        <CardTransacoes />
-        <CardTransacoes />
       </ScrollView>
-      <Modal
+      <CardModalMoeda
         isVisible={isModalVisible}
-        animationIn="slideInUp"
-        animationOut="slideOutDown"
-        backdropColor="black"
-        backdropOpacity={0.5}
-        onBackdropPress={toggleModal}>
-        <View style={styles.modalContent}>
-          <AppText size="huge" style={styles.modalTitle} color="black">
-            Selecione uma Moeda
-          </AppText>
-          {coins.map(coin => (
-            <TouchableOpacity
-              key={coin.id}
-              style={styles.coinItem}
-              onPress={() => {
-                setSelectedCoin(coin.name);
-                toggleModal();
-              }}>
-              <CollectionSvgImg iconName={coin.value} width={15} height={15} />
-              <AppText style={{marginLeft: 5}}>{coin.name}</AppText>
-            </TouchableOpacity>
-          ))}
-        </View>
-      </Modal>
+        toggleModal={toggleModal}
+        setSelectedCoin={setSelectedCoin}
+        coins={coins}
+      />
     </View>
   );
 };
@@ -91,20 +56,4 @@ export default ScreenWallet;
 const styles = StyleSheet.create({
   container: {flex: 1, backgroundColor: '#1a1d1b', padding: 0},
   transacoes: {marginTop: 15},
-  modalContent: {
-    backgroundColor: 'white',
-    padding: 20,
-    borderRadius: 10,
-  },
-  modalTitle: {
-    marginBottom: 10,
-    textAlign: 'center',
-  },
-  coinItem: {
-    padding: 10,
-    borderBottomWidth: 1,
-    borderBottomColor: '#ccc',
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
 });
