b76bddc47c1dfb2e591e86b69bee52f68511a6f0
GEOVANI\conta <geosegat@gmail.com>
2024-08-01 14:16:48 -0300

    no message

--- src/Telas/ScreenWallet/index.tsx
+++ src/Telas/ScreenWallet/index.tsx
@@ -3,24 +3,24 @@ import React, {useState} from 'react';
 import {NavigationProps} from '../../utilitis/types/navigation';
 import AppText from '../../Componentes/AppText';
 import CardMoedasWallet from '../../Componentes/CardMoedasWallet';
-import CardBalanceCritpomoeda from '../../Componentes/CardBalanceCriptomoeda';
+import CardBalanceCriptomoeda from '../../Componentes/CardBalanceCriptomoeda';
 import CardTransacoes from '../../Componentes/CardTransacoes';
 import CardModalMoeda, {Coin} from '../../Componentes/CardModalMoeda';
 
 const ScreenWallet: React.FC<NavigationProps> = ({navigation}) => {
-  const [valorRecebido, setValoRecebido] = useState<number>(0.00006769);
+  const [valorRecebido, setValorRecebido] = useState<number>(0.00006769);
   const [isModalVisible, setModalVisible] = useState(false);
-  const [selectedCoin, setSelectedCoin] = useState<string>('');
+  const [selectedCoin, setSelectedCoin] = useState<Coin | null>(null);
 
   const toggleModal = () => {
     setModalVisible(!isModalVisible);
   };
 
   const coins: Coin[] = [
-    {id: '1', name: 'BTC', value: 'bitImg'},
-    {id: '2', name: 'ETH', value: 'ethImg'},
-    {id: '3', name: 'ARK', value: 'arkImg'},
-    {id: '4', name: 'DASH', value: 'dashImg'},
+    {id: '1', name: 'Bitcoin', value: 'bitImg', sigla: 'btc'},
+    {id: '2', name: 'DogeCoin', value: 'dogeImg', sigla: 'dog'},
+    {id: '3', name: 'Ark', value: 'arkImg', sigla: 'ark'},
+    {id: '4', name: 'Dash', value: 'dashImg', sigla: 'dash'},
   ];
 
   return (
@@ -29,13 +29,18 @@ const ScreenWallet: React.FC<NavigationProps> = ({navigation}) => {
         <View style={{height: 25}} />
         <CardMoedasWallet onPress={toggleModal} />
         <View style={{height: 25}} />
-        <CardBalanceCritpomoeda />
+        <CardBalanceCriptomoeda
+          iconName={selectedCoin ? selectedCoin.value : 'arkImg'}
+          moeda={selectedCoin ? selectedCoin.name : 'Ark'}
+          balance={36.13003924}
+          criptoSigla={selectedCoin ? selectedCoin.sigla : 'ark'}
+        />
         <AppText size="huge" style={styles.transacoes} color="white">
           Transações
         </AppText>
       </View>
       <ScrollView>
-        <CardTransacoes iconName="ethImg" moeda="eth" value={0.00136113} />
+        <CardTransacoes iconName="dogeImg" moeda="dog" value={36.13003924} />
         <CardTransacoes iconName="arkImg" moeda="ark" value={11.72506138} />
         <CardTransacoes iconName="bitImg" value={0.16565596} />
         <CardTransacoes iconName="dashImg" moeda="dash" value={valorRecebido} />
